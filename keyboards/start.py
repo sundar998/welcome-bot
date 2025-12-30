510334f530efe9c52cfe587f3dbf65cdcedfb6c0
sundar998 <sundar19921@gmail.com>
2025-12-30 16:46:58 +0530

    Update start.py

--- keyboards/start.py
+++ keyboards/start.py
@@ -1,12 +1,8 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def start_keyboard():
-    return InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton(
-                "➕ Add me to a group",
-                url="https://t.me/Demodemo1617_robot?startgroup=true"
-            )
-        ]]
-    )
-
+    buttons = [
+        [InlineKeyboardButton("➕ Add me to a group", url="https://t.me/Demodemo1617_robot?startgroup=true")],
+        [InlineKeyboardButton("⚙️ Manage group settings", callback_data="manage_groups")]
+    ]
+    return InlineKeyboardMarkup(buttons)

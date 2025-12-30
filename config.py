4c61d5c04c67095caabe542c2a28d2176ccbf5a8
sundar998 <sundar19921@gmail.com>
2025-12-30 18:08:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,5 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-OWNER_ID = int(os.getenv("8054916377"))   # REQUIRED
+OWNER_ID = int(os.getenv("OWNER_ID"))      # ✅ FIXED
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL")) if os.getenv("LOG_CHANNEL") else None
-

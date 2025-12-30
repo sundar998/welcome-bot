1affecebd24a2ad0a75eba1e04be50e012648abd
sundar998 <sundar19921@gmail.com>
2025-12-30 17:33:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,8 @@
 import os
 
-# ---------------------------
-# Telegram API
-# ---------------------------
-API_ID = int(os.getenv("API_ID", ""))         # Replace with your API_ID
-API_HASH = os.getenv("API_HASH", "")  # Replace with your API_HASH
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")  # Replace with your bot token
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ""))
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# ---------------------------
-# MongoDB
-# ---------------------------
-MONGO_URI = os.getenv("")
-
-# ---------------------------
-# Owner / Admin
-# ---------------------------
-OWNER_ID = os.getenv("")  # Telegram user ID of the bot owner
-
-
+OWNER_ID = int(os.getenv("OWNER_ID"))   # REQUIRED
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL")) if os.getenv("LOG_CHANNEL") else None

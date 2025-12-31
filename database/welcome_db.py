374c034287e9975918f1b60c2f2dd58cbd6a19c7
sundar998 <sundar19921@gmail.com>
2025-12-31 10:38:40 +0530

    Update welcome_db.py

--- database/welcome_db.py
+++ database/welcome_db.py
@@ -1,34 +1,27 @@
-from .mongo import welcome_col
+from database.mongo import welcome_col
 
-def set_welcome_text(group_id: int, text: str):
+def set_welcome_media(group_id, file_id, caption=None):
     welcome_col.update_one(
         {"_id": group_id},
-        {"$set": {"text": text}},
+        {"$set": {"media": {"file_id": file_id, "caption": caption}}},
         upsert=True
     )
 
-def get_welcome_text(group_id: int):
-    doc = welcome_col.find_one({"_id": group_id})
-    return doc.get("text") if doc else None
+def get_welcome_media(group_id):
+    data = welcome_col.find_one({"_id": group_id})
+    return data.get("media") if data else None
 
-def set_welcome_media(group_id: int, media_type: str, file_id: str, caption: str = None):
-    welcome_col.update_one(
-        {"_id": group_id},
-        {"$set": {"media": {"type": media_type, "file_id": file_id, "caption": caption}}},
-        upsert=True
-    )
-
-def get_welcome_media(group_id: int):
-    doc = welcome_col.find_one({"_id": group_id})
-    return doc.get("media") if doc else None
-
-def set_buttons(group_id: int, buttons: list):
+def set_buttons(group_id, buttons):
     welcome_col.update_one(
         {"_id": group_id},
         {"$set": {"buttons": buttons}},
         upsert=True
     )
 
-def get_buttons(group_id: int):
-    doc = welcome_col.find_one({"_id": group_id})
-    return doc.get("buttons") if doc else []
+def get_buttons(group_id):
+    data = welcome_col.find_one({"_id": group_id})
+    return data.get("buttons", []) if data else []
+
+def get_welcome_text(group_id):
+    data = welcome_col.find_one({"_id": group_id})
+    return data.get("text") if data else None
